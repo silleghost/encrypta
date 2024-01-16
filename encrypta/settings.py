715a0bffc9d25e43b86b73bde13daf849dcfb27f
Alex <silleghost@gmail.com>
2024-01-16 17:04:38 +0300

    Model setup

--- encrypta/settings.py
+++ encrypta/settings.py
@@ -37,8 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.postgres',
 
-    "vault",
+    'debug_toolbar',
+
+    'vault',
 ]
 
 MIDDLEWARE = [
@@ -49,6 +52,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'encrypta.urls'
@@ -77,8 +82,12 @@ WSGI_APPLICATION = 'encrypta.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'encrypta',
+        'USER': 'AlexDjango',
+        'PASSWORD': 'home',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
--- vault/models.py
+++ vault/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.db.models.functions import Now
 
-# Create your models here.
+
+class Categories(models.Model):
+    name = models.CharField(max_length=150, unique=True, verbose_name="Название категории")
+
+    class Meta:
+        db_table = "category"
+        verbose_name = "Категорию"
+        verbose_name_plural = "Категории"
+        ordering = ("name",)
+
+    def __str__(self):
+        return self.name
+
+
+class Passwords(models.Model):
+    app_name = models.CharField(max_length=150, verbose_name="Название приложения")
+    username = models.CharField(max_length=150, verbose_name="Имя пользователя")
+    password = models.JSONField(max_length=300, verbose_name="Пароль")
+    urls = models.JSONField(max_length=500, verbose_name="Адреса")
+    category = models.ForeignKey(to=Categories, on_delete=models.CASCADE)
+    creation_date = models.DateTimeField(db_default=Now())
