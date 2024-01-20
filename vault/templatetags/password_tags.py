bdaf80e451a4ff175d3049318b0d407f2f077668
Alex <silleghost@gmail.com>
2024-01-20 16:13:50 +0300

    template tag for collecting data

--- vault/templatetags/password_tags.py
+++ vault/templatetags/password_tags.py
@@ -1,7 +1,16 @@
 from django import template
 
+from vault.models import Records
+
 register = template.Library()
 
 @register.simple_tag
 def get_data_from_JSON_file(dict, key):
     return dict[key]
+
+
+@register.simple_tag
+def get_password_for_record(name):
+    target_app = Records.objects.get(app_name=name)
+    target_record = target_app.records
+    return target_record.password
