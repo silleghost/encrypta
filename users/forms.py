0593e9ef72716c5ed1c5218b1b7992afaf061872
Alyx <sillehost@gmail.com>
2024-02-23 11:05:59 +0300

    Added encryption functions

--- users/forms.py
+++ users/forms.py
@@ -1,5 +1,6 @@
 from django import forms
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.hashers import make_password
 
 from django.contrib.auth.password_validation import validate_password
 from django.core.validators import validate_email
@@ -16,46 +17,60 @@ class UserLoginForm(AuthenticationForm):
         fields = ["username", "password"]
 
 
-class UserRegistrationForm(forms.Form):
+class UserRegistrationForm(UserCreationForm):
+
+    class Meta:
+        model = User
+        fields = ["username", "email", "password1", "password2"]
+
     username = forms.CharField()
     email = forms.CharField()
     password1 = forms.CharField()
     password2 = forms.CharField()
 
-    def clean_username(self):
-        username = self.cleaned_data["username"]
-        if User.objects.filter(username__iexact=username).exists():
-            raise forms.ValidationError(
-                "Пользователь с таким именем уже существует", code="unique"
-            )
-        return username
+    # def clean_username(self):
+    #     username = self.cleaned_data["username"]
+    #     if User.objects.filter(username__iexact=username).exists():
+    #         raise forms.ValidationError(
+    #             "Пользователь с таким именем уже существует", code="unique"
+    #         )
+    #     return username
 
-    def clean_email(self):
-        email = self.cleaned_data["email"]
-        try:
-            validate_email(email)
-        except:
-            raise forms.ValidationError("Неправильный email", code="invalid")
-        if User.objects.filter(email=email).exists():
-            raise forms.ValidationError(
-                "Пользователь с таким email уже существует", code="unique"
-            )
-        return email
+    # def clean_email(self):
+    #     email = self.cleaned_data["email"]
+    #     try:
+    #         validate_email(email)
+    #     except:
+    #         raise forms.ValidationError("Неправильный email", code="invalid")
+    #     if User.objects.filter(email=email).exists():
+    #         raise forms.ValidationError(
+    #             "Пользователь с таким email уже существует", code="unique"
+    #         )
+    #     return email
 
-    def clean_password1(self):
-        password1 = self.cleaned_data["password1"]
-        try:
-            validate_password(password1)
-        except forms.ValidationError as error:
-            raise forms.ValidationError(error.messages)
-        return password1
+    # def clean_password1(self):
+    #     password1 = self.cleaned_data["password1"]
+    #     try:
+    #         validate_password(password1)
+    #     except forms.ValidationError as error:
+    #         raise forms.ValidationError(error.messages)
+    #     return password1
 
-    def clean_password2(self):
-        password1 = self.cleaned_data.get("password1")
-        password2 = self.cleaned_data.get("password2")
-        if password1 and password2 and password1 != password2:
-            raise forms.ValidationError("Пароли не совпадают")
-        return password2
+    # def clean_password2(self):
+    #     password1 = self.cleaned_data.get("password1")
+    #     password2 = self.cleaned_data.get("password2")
+    #     if password1 and password2 and password1 != password2:
+    #         raise forms.ValidationError("Пароли не совпадают")
+    #     return password2
+
+    # def save(self):
+    #     username = self.cleaned_data["username"]
+    #     password = self.cleaned_data["password1"]
+    #     email = self.cleaned_data["email"]
+
+    #     hashed_password = make_password(password)
+    #     user = User(username=username, password=hashed_password, email=email)
+    #     user.save()
 
 
 class UserSettingsForm(forms.Form):
---
+++ vault/utils.py
@@ -0,0 +1,18 @@
+from base64 import b64decode
+from base64 import b64encode
+
+from Crypto.Cipher import AES
+from Crypto.Random import get_random_bytes
+from Crypto.Util.Padding import pad, unpad
+
+
+def encrypt(data, key):
+    iv = get_random_bytes(AES.block_size)
+    cipher = AES.new(key, AES.MODE_CBC, iv)
+    return b64encode(iv + cipher.encrypt(pad(data.encode("utf-8"))), AES.block_size)
+
+
+def decrypt(data, key):
+    raw = b64decode(data)
+    cipher = AES.new(key, AES.MODE_CBC, raw[:AES.block_size])
+    return unpad(cipher.decrypt(raw[AES.block_size:]), AES.block_size)
